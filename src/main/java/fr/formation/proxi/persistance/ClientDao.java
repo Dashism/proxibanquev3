fd39f2958fedd9aff988137255eda9badfb202f0
Dashism <benoitdarenne@hotmail.fr>
2018-12-17 17:42:59 +0100

    lecture client ???

--- src/main/java/fr/formation/proxi/persistance/ClientDao.java
+++ src/main/java/fr/formation/proxi/persistance/ClientDao.java
@@ -2,6 +2,8 @@ package fr.formation.proxi.persistance;
 
 import java.util.List;
 
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.TypedQuery;
 
 import fr.formation.proxi.metier.entity.Client;
@@ -10,10 +12,9 @@ public class ClientDao extends AbstractDao<Client> {
 
 	@Override
 	public Client read(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.read(id, new Client());
 	}
-
+	
 	@Override
 	public List<Client> readAll() {
 		// TODO Auto-generated method stub
@@ -21,24 +22,15 @@ public class ClientDao extends AbstractDao<Client> {
 	}
 
 	public Client readIdByName(String firstname, String lastname) {
-		Client result1 = null;
-		Client result2 = null;
-		TypedQuery<Client> query1 = this.em.createQuery(JpqlQueries.SELECT_CLIENT_BY_NAME, Client.class);
-		query1.setParameter("firstname", firstname);
-		query1.setParameter("lastname", lastname);
-		result1 = query1.getSingleResult();
-		TypedQuery<Client> query2 = this.em.createQuery(JpqlQueries.SELECT_CLIENT_BY_NAME, Client.class);
-		query2.setParameter("firstname", lastname);
-		query2.setParameter("lastname", firstname);
-		result2 = query2.getSingleResult();
-		if (result1 != null && result2 != null) {
-			return null;
-		} else if (result1 != null && result2 == null) {
-			return result1;
-		} else if (result1 == null && result2 != null) {
-			return result2;
-		} else {
-			return null;
+		Client result = null;
+		TypedQuery<Client> query = this.em.createQuery(JpqlQueries.SELECT_CLIENT_BY_NAME, Client.class);
+		query.setParameter("firstname", firstname);
+		query.setParameter("lastname", lastname);
+		try {
+			result = query.getSingleResult();
+		} catch (NoResultException | NonUniqueResultException e) {
+			System.out.println("Pas de client avec ces parametres.");
 		}
+		return result;
 	}
 }
