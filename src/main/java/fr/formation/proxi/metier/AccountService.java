7013a6624babdd36dd6ed2aaefd66517b4f584aa
Chocolaterules <marie.boulez@laposte.net>
2018-12-17 16:14:18 +0100

    Couche metier service

--- src/main/java/fr/formation/proxi/metier/AccountService.java
+++ src/main/java/fr/formation/proxi/metier/AccountService.java
@@ -3,50 +3,55 @@ package fr.formation.proxi.metier;
 import java.util.List;
 
 import fr.formation.proxi.metier.entity.Account;
+import fr.formation.proxi.metier.entity.BankCard;
+import fr.formation.proxi.metier.entity.Chequebook;
+import fr.formation.proxi.metier.entity.CurrentAccount;
 import fr.formation.proxi.persistance.AccountDao;
 
 /**
- * la classe AccountService definit toutes les mÃ©thodes de manipulation des comptes
+ * la classe AccountService definit toutes les mÃ©thodes de manipulation des
+ * comptes
  * 
  * @author Marie_Julien
  *
  */
 
 public class AccountService {
-	
+
 	/**
 	 * Permet de faire un singleton.
 	 */
 	public static final AccountService INSTANCE = new AccountService();
-	
+
 	public static AccountService getInstance() {
 		return AccountService.INSTANCE;
 	}
-	
-	
-	
+
 	/**
-	 * Permet de faire appel aux méthodes de la classe AccountDao.
+	 * Permet de faire appel aux mï¿½thodes de la classe AccountDao.
 	 */
 	private final AccountDao dao;
 
 	public AccountService() {
 		this.dao = new AccountDao();
 	}
-	
+
 	/**
-	 * Ajoute un compte à un client défini
-	 * @param number Le numéro de compte du compte à créer.
-	 * @param balance Le solde du compte à créer.
-	 * @param savings Si vrai, le compte à créer est un compte épargne, si faux le compte à créer est un compte courant.
-	 * @param id L'identifiant du client auquel le compte est rattaché.
+	 * Ajoute un compte ï¿½ un client dï¿½fini
+	 * 
+	 * @param number  Le numï¿½ro de compte du compte ï¿½ crï¿½er.
+	 * @param balance Le solde du compte ï¿½ crï¿½er.
+	 * @param savings Si vrai, le compte ï¿½ crï¿½er est un compte ï¿½pargne, si faux le
+	 *                compte ï¿½ crï¿½er est un compte courant.
+	 * @param id      L'identifiant du client auquel le compte est rattachï¿½.
 	 */
-	public void addAccount(String number, Float balance, Boolean savings, Integer id) {
-		this.dao.create(new Account(number, balance, savings), id);
-	}
-	
+//	public void addAccount(Integer id, String number, Float balance, String label, String openingDate, Chequebook chequebook) {
+//		this.dao.create(new Account(number, balance, savings), id);
+//	}
+//	
 	/**
-	 * Récupère la liste des comptes d'un client défini.
+	 * Rï¿½cupï¿½re la liste des comptes d'un client dï¿½fini.
+	 * 
 	 * @param id L'identifiant du client dont il faut afficher les comptes
 	 * @return Renvoie la liste des comptes.
 	 */
@@ -55,28 +60,66 @@ public class AccountService {
 	}
 
 	/**
-	 * Fait un virement du compte A au compte B d'un montant choisi, si le solde du compte A est suffisant et les comptes A et B sont différents.
-	 * @param compteA L'identifiant du compte à débiter.
-	 * @param compteB L'identifiant du compte à créditer.
-	 * @param montant Le montant à virer
-	 * @return Renvoie true si le virement a réussi, false s'il a échoué.
+	 * Fait un virement du compte A au compte B d'un montant choisi, si le solde du
+	 * compte A est suffisant et les comptes A et B sont diffï¿½rents.
+	 * 
+	 * @param compteA L'identifiant du compte ï¿½ dï¿½biter.
+	 * @param compteB L'identifiant du compte ï¿½ crï¿½diter.
+	 * @param montant Le montant ï¿½ virer
+	 * @return Renvoie true si le virement a rï¿½ussi, false s'il a ï¿½chouï¿½.
 	 */
-	public boolean transfer(Integer compteA, Integer compteB, Float montant) {	
-		Account accountA  = this.dao.read(compteA);
-		Account accountB  = this.dao.read(compteB);
+	public boolean transfer(Integer compteA, Integer compteB, Float montant) {
+		Account accountA = this.dao.read(compteA);
+		Account accountB = this.dao.read(compteB);
 		Boolean result = false;
-		if (accountA.getId()!=accountB.getId()) {
-			if (accountA.getBalance() >= montant) {
-				accountA.setBalance(accountA.getBalance() - montant);
-				this.dao.update(accountA);
-				accountB.setBalance(accountB.getBalance() + montant);
-				this.dao.update(accountB);
+		if (montant <= 900) {
+			if (accountA.getId() != accountB.getId()) {
+				if (accountA.getBalance() >= montant) {
+					accountA.setBalance(accountA.getBalance() - montant);
+					this.dao.update(accountA);
+					accountB.setBalance(accountB.getBalance() + montant);
+					this.dao.update(accountB);
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public boolean cashWithdrawal(Integer accountId, Float amount) {
+		Account account = this.dao.read(accountId);
+		Boolean result = false;
+		if (account instanceof CurrentAccount && amount <= 300 && account.getBalance() >= amount) {
+			account.setBalance(account.getBalance() - amount);
+			this.dao.update(account);
+			result = true;
+		}
+		return result;
+	}
+	
+	public boolean newCard (Integer accountId, BankCard bankCard) {
+		Account account = this.dao.read(accountId);
+		Boolean result = false;
+		if (account instanceof CurrentAccount) {
+			CurrentAccount curaccount = (CurrentAccount) account;
+			if (curaccount.getCard() == null) {
+				curaccount.setCard(bankCard);
+				this.dao.update(curaccount);
 				result = true;
 			}
 		}
 		return result;
-		
 	}
-
+	
+	public boolean newChequebook (Integer accountId, Chequebook chequebook) {
+		Account account = this.dao.read(accountId);
+		Boolean result = false;
+		if (account.getChequebook() == null) {
+			account.setChequebook(chequebook);
+			this.dao.update(account);
+			result = true;
+		}
+		return result;
+	}
 
 }
